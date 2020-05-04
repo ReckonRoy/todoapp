62776868f977b29d9e9fb246946a52199920525b
Reckon Roy <jongweleroy@yahoo.com>
2020-05-04 02:56:40 +0200

    modified login.php

--- login.php
+++ login.php
@@ -5,8 +5,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-$db_hostname = 'localhost';
-$db_database = 'timeless';
-$db_username = 'LeRoy';
-$db_password = 'ReckonR11';
+$db_hostname = 'http://167.71.74.228/phpmyadmin';
+$db_database = 'WDI2020_leRoy';
+$db_username = 'leRoy';
+$db_password = 'Vapid87kept';
 ?>
